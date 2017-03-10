094e3d5e9dbe6ea1069e66a7d8c11e656bf6ef6c
Sijawusz Pur Rahnama <sija@sija.pl>
2017-03-10 14:48:30 +0100

    Add raven.cr dependency

--- src/gphoto2-web.cr
+++ src/gphoto2-web.cr
@@ -4,9 +4,21 @@ require "zip"
 require "gphoto2"
 require "kemal"
 require "pool/connection"
+require "raven"
+require "raven/integrations/kemal"
 
 require "./ext/*"
 require "./gphoto2/web"
 
-Kemal.config.add_handler GPhoto2::Web::RemovePoweredByHeaderHandler.new
+Raven.configure do |config|
+  config.async = true
+end
+
+Kemal.config do |config|
+  config.logger = Raven::Kemal::LogHandler.new(Kemal::CommonLogHandler.new)
+
+  config.add_handler Raven::Kemal::ExceptionHandler.new
+  config.add_handler GPhoto2::Web::RemovePoweredByHeaderHandler.new
+end
+
 Kemal.run
