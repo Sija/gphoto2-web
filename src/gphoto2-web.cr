10988470652741564155b9469eb1a7b6d7f05f77
Sijawusz Pur Rahnama <sija@sija.pl>
2017-03-10 14:46:57 +0100

    Extract RemovePoweredByHeaderHandler to its own file

--- src/gphoto2-web.cr
+++ src/gphoto2-web.cr
@@ -8,18 +8,5 @@ require "pool/connection"
 require "./ext/*"
 require "./gphoto2/web"
 
-module GPhoto2
-  module Web
-    class RemovePoweredByHeaderHandler
-      include HTTP::Handler
-
-      def call(context)
-        context.response.headers.delete "X-Powered-By"
-        call_next context
-      end
-    end
-  end
-end
-
 Kemal.config.add_handler GPhoto2::Web::RemovePoweredByHeaderHandler.new
 Kemal.run
