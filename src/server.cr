b72ea436ac6b0efc25ad211fd7a52065b993b3ff
Sijawusz Pur Rahnama <sija@sija.pl>
2020-08-06 01:12:14 +0200

    Add Raven’s Log integration

--- src/server.cr
+++ src/server.cr
@@ -1,5 +1,17 @@
 require "./gphoto2-web"
 
+production = Kemal.config.env == "production"
+
+Log.setup do |c|
+  severity =
+    production ? Log::Severity::Info : Log::Severity::Debug
+
+  c.bind "raven.*", severity, Log::IOBackend.new
+  c.bind "*", :debug, Raven::LogBackend.new(record_breadcrumbs: true)
+  c.bind "*", :warn, Raven::LogBackend.new(capture_exceptions: true)
+  c.bind "*", :fatal, Raven::LogBackend.new(capture_all: true)
+end
+
 Raven.configure do |config|
   config.async = true
   config.current_environment = Kemal.config.env
